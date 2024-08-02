de0ed03404f685f1afa179339b81e3f78d0410f6
bbel <bweb33@protonmail.com>
2024-08-02 13:08:31 +0200

    first

--- markdown2html.py
+++ markdown2html.py
@@ -6,13 +6,17 @@
 import sys
 import os
 
-if __name__ == "__main__":
+def test_nb_arg(argv=sys.argv){
     if len(sys.argv) != 3:
         print(
             'Usage: ./markdown2html.py README.md README.html',
             file=sys.stderr
         )
         sys.exit(1)
+}
+
+if __name__ == "__main__":
+    test_nb_arg()
     mdFile = sys.argv[1]
     htmlFile = sys.argv[2]
     if not os.path.exists(mdFile):
