cc954996957f1b70d666d4042fe7113480576d79
bbel <bweb33@protonmail.com>
2024-08-02 13:11:08 +0200

    first

--- markdown2html.py
+++ markdown2html.py
@@ -6,14 +6,13 @@
 import sys
 import os
 
-def test_nb_arg(argv=sys.argv){
+def test_nb_arg(argv=sys.argv):
     if len(sys.argv) != 3:
         print(
             'Usage: ./markdown2html.py README.md README.html',
             file=sys.stderr
         )
         sys.exit(1)
-}
 
 if __name__ == "__main__":
     test_nb_arg()
