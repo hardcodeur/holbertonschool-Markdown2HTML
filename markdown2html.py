959675c8b440099635bdfcd6505b83b9c0c3c532
bbel <bweb33@protonmail.com>
2024-08-02 10:37:28 +0200

    first commit

--- markdown2html.py
+++ markdown2html.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 import sys
 import os
-
 """
-markdown2html.py  
-Write a script markdown2html.py that takes an argument 2 strings
+    markdown2html.py
+    Write a script markdown2html.py that takes an argument 2 strings
 """
-
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+        print(
+            'Usage: ./markdown2html.py README.md README.html',
+            file=sys.stderr
+        )
         sys.exit(1)
-    else :
+    else:
         mdFile = sys.argv[1]
         htmlFile = sys.argv[2]
 
         if not os.path.exists(mdFile):
             print('Missing <filename>', file=sys.stderr)
             sys.exit(1)
-        
         print("")
         exit(0)
-        
-        
