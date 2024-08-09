c59edb8f94564ce68f77928091894c455c30f629
bbel <bweb33@protonmail.com>
2024-08-09 13:51:00 +0200

    exo 1

--- markdown2html.py
+++ markdown2html.py
@@ -8,14 +8,15 @@ import os
 import re
 
 if __name__ == "__main__":
+    
     if len(sys.argv) <= 2:
         print('Usage: ./markdown2html.py README.md README.html',file=sys.stderr)
         sys.exit(1)
-    else: 
-        if not os.path.exists(sys.argv[2]):
-            print(f'Missing {sys.argv[2]}', file=sys.stderr)
-            sys.exit(1)
-        else:
-            print("")
-            sys.exit(0)
+     
+    if not os.path.exists(sys.argv[2]):
+        print(f'Missing {sys.argv[2]}', file=sys.stderr)
+        sys.exit(1)
+    
+    print("")
+    sys.exit(0)
     
