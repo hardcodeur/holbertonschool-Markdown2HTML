201838f0d688fcaf087a0e5de8e0df5d584fded9
bbel <bweb33@protonmail.com>
2024-08-09 13:56:34 +0200

    exo 1

--- markdown2html.py
+++ markdown2html.py
@@ -9,7 +9,7 @@ import re
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print('Usage: ./markdown2html.py README.md README.html',file=sys.stderr)
         sys.exit(1)
      
